47ca942edee20c4e2ba80fed53a257ec166c15ff
aman satyawani <amansatyawani10@gmail.com>
2024-10-18 15:08:01 +0530

    two pointer final commit

--- src/twoPointer/NextPermutation.java
+++ src/twoPointer/NextPermutation.java
@@ -1,2 +1,32 @@
-package twoPointer;public class NextPermutation {
+package twoPointer;
+
+public class NextPermutation {
+    public void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public void reverse(int[] nums,int start){
+        int i=start;
+        int j=nums.length-1;
+        while(i<j){
+            swap(nums,i,j);
+            i++;
+            j--;
+        }
+    }
+    public void nextPermutation(int[] nums) {
+        int i=nums.length-2;
+        while(i>=0 && nums[i+1]<=nums[i]){
+            i--;
+        }
+        if(i>=0){
+            int j=nums.length-1;
+            while(nums[j]<=nums[i]){
+                j--;
+            }
+            swap(nums,i,j);
+        }
+        reverse(nums,i+1);
+    }
 }
